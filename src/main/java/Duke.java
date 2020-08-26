0eb2e55d040b8507b96e658bd23759826268ed8b
Brendan <bwsjsg@gmail.com>
2020-08-26 21:32:13 +0800

    Level-02: Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,8 +9,11 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         */
+        int count = 0;
         String input;
-        System.out.println("Hi! My name is Duke!\n" + "How can I help you?");
+        String[] list = new String[100];
+
+        System.out.println("Hi! My name is Bop!\n" + "How can I help you?");
 
         // Echo loops until "bye" is inputted
         while(true) {
@@ -20,9 +23,23 @@ public class Duke {
                 System.out.println("Goodbye. See you soon!"); // Exits when "bye" is inputted
                 break;
             }
-            System.out.println(input);
+            else if (input.equals("list")){
+                int listCount = 1;
+                for (String item: list){
+                    if (item == null) {
+                        if (listCount == 1)
+                            System.out.println("The list is empty.");
+                        break;
+                    }
+                    System.out.println(listCount + ". " + item);
+                    listCount++;
+                }
+            }
+            else {
+                list[count] = input;
+                count++;
+                System.out.println("added: " + input);
+            }
         }
-
-
     }
 }
