c3304533489caa302891bf7840824489348ee58e
Brendan <bwsjsg@gmail.com>
2020-08-26 23:48:22 +0800

    Level-03: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,7 +11,7 @@ public class Duke {
         */
         int count = 0;
         String input;
-        String[] list = new String[100];
+        Task list[] = new Task[100];
 
         System.out.println("Hi! My name is Bop!\n" + "How can I help you?");
 
@@ -19,24 +19,39 @@ public class Duke {
         while(true) {
             Scanner echo = new Scanner(System.in); // Scan and echo user input
             input = echo.nextLine();
+
+            // Exit program if "bye" is inputted
             if (input.equals("bye")) {
-                System.out.println("Goodbye. See you soon!"); // Exits when "bye" is inputted
+                System.out.println("Goodbye. See you soon!");
                 break;
             }
+
+            // Print list of tasks
             else if (input.equals("list")){
                 int listCount = 1;
-                for (String item: list){
+                for (Task item: list){
                     if (item == null) {
                         if (listCount == 1)
                             System.out.println("The list is empty.");
                         break;
                     }
-                    System.out.println(listCount + ". " + item);
+                    System.out.println(listCount + ".[" + item.getStatusIcon() + "] " + item.description);
                     listCount++;
                 }
             }
+
+            // Set selected task as done
+            else if (input.contains("done")){
+                // The number of the task that is done
+                int doneTask = Integer.parseInt(input.replaceAll("[^0-9]", "")) - 1;
+                list[doneTask].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + "  ["
+                        + list[doneTask].getStatusIcon() + "] " +list[doneTask].getDescription());
+            }
+
+            // Add input as new task
             else {
-                list[count] = input;
+                list[count] = new Task(input);
                 count++;
                 System.out.println("added: " + input);
             }
